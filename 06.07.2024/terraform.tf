a00760cbf3550f6b6f7c1d9407732360e56389ae
ed charleson <edward.charleson@gmail.com>
2024-06-08 21:46:07 +0000

    emaca06082024

--- 06.07.2024/terraform.tf
+++ 06.07.2024/terraform.tf
@@ -1,15 +1,16 @@
 terraform {
+ 
+ backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "emaca23_demo"
 
-  backend "s3" {
-    bucket = "my-terraform-state-ejc"
-    key    = "path/to/my/key"
-    region = "us-east-1"
+    workspaces {
+      name = "my-aws-app"
+    }
+   }
+  
+  required_version = ">= 0.15.1"
     
-    dynamodb_table = "terraform-locs"
-    encrypt = true
-  }
-
-  required_version = ">= 0.15.0"
 
   required_providers {
     aws = {
@@ -18,47 +19,32 @@ terraform {
       version = "~> 5.0"
     }
 
-
-  required_version = ">= 0.15.1"
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-
     http = {
       source  = "hashicorp/http"
       version = "2.1.0"
     }
+    
     random = {
       source  = "hashicorp/random"
       version = "3.1.0"
     }
+    
     local = {
       source  = "hashicorp/local"
       version = "2.1.0"
     }
+    
     tls = {
-
       source = "hashicorp/tls"
 
       version = "4.0.5"
 
-    }
-  }
+       }
 
-      source  = "hashicorp/tls"
-      version = "3.1.0"
-    }
+       
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "2.57.0"
     }
-  }
-}
-
-terraform {
-  backend "local" {
-    path = "mystate/terraform.tfstate"
-  }
-
-}
+    }   
+    }
