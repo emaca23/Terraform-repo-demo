c19d5d129eeef50786b4db83fd9c12351aac9e8e
ed charleson <edward.charleson@gmail.com>
2024-06-15 20:34:30 +0000

    emaca2306152024

---
+++ project.2/main3.tf
@@ -0,0 +1,77 @@
+# Configure the AWS Provider
+provider "aws" {}
+
+resource "aws_launch_template" "project_2" {
+  name          = "project2"
+  image_id      = "ami-00beae93a2d981137"
+  instance_type = "t2.micro"
+  user_data     = filebase64("user_data.sh")
+ 
+  
+  network_interfaces {
+    security_groups = ["${aws_security_group.my-new-security-group.id}"] 
+    associate_public_ip_address = true
+  }
+  
+  tags = {
+    Name = "test"
+  }
+}
+
+resource "aws_autoscaling_group" "p2_demo" {
+  desired_capacity     = 2
+  max_size             = 5
+  min_size             = 2
+  #launch_configuration = "${aws_launch_template.project_2.id}"
+  vpc_zone_identifier  = ["subnet-003ffee4f109276c5", "subnet-018af8d8fe5ff564c"]
+  
+  launch_template {
+    id      = aws_launch_template.project_2.id
+    version = "$Latest"
+  }
+}
+
+resource "aws_s3_bucket" "bucket_p2" {
+  bucket = "p2-test-bucket-emaca2312"
+
+  tags = {
+    Name    = "My S3 Bucket"
+    Purpose = "Project bucket for terraform_asg"
+  }
+}
+
+resource "aws_security_group" "my-new-security-group" {
+  name        = "p2_security_group"
+  description = "Allow inbound traffic on tcp 22 and tcp 8080"
+  vpc_id      = "vpc-0ca271afb22e66917"
+
+  ingress {
+    description = "Allow all from public"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  #ingress {
+   # description = "Allow all from public 443"
+   # from_port   = 443
+   # to_port     = 443
+   # protocol    = "tcp"
+   # cidr_blocks = ["0.0.0.0/0"]
+ # }
+ 
+   # ingress {
+   # description = "Allow all from public 22"
+   # from_port   = 22
+   # to_port     = 22
+   # protocol    = "tcp"
+   # cidr_blocks = ["73.229.203.54/32"]
+ # }
+  
+  
+
+  tags = {
+    Name    = "web_server_inbound"
+    Purpose = "80 and 443 inbound"
+  }
+}
